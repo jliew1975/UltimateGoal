993ca19ee3f03918b22623f8e3c3ce9bbb046109
jliew1975 <jliew1975@gmail.com>
2019-10-01 20:13:29 -0400

    Fixed UIPickerView issue.

--- XCode/ScoutingApp/ScoutingApp/ViewController.swift
+++ XCode/ScoutingApp/ScoutingApp/ViewController.swift
@@ -14,21 +14,23 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     // Connect objects to view controller
     @IBOutlet weak var eventPickerView: UIPickerView!
     
+    
     var ref:DatabaseReference?
     var databaseHandle:DatabaseHandle?
     
     // Define variables
-    var glibs = [String]()
+    var eventNames = [String]()
     
     // Use functions necessary for pickerview
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return glibs[row]
+        return eventNames[row]
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return glibs.count
+        return eventNames.count
     }
     
     // The normal XCode stuff
@@ -36,25 +38,34 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // bind the data
+        self.eventPickerView.delegate = self
+        self.eventPickerView.dataSource = self
         
         // Retrieve the posts and listen for any changes to the Events branch
         databaseHandle = theApp.sharedInstance.fbref.child("Events").observe(.childAdded, with: { (snapshot) in
             
             let eventName = snapshot.childSnapshot(forPath: "EventName").value as? String
+            
             if let eventName = eventName {
-                self.glibs.append(eventName)
+                self.eventNames.append(eventName)
             }
             
+            // once the array is loaded we need to notify the
+            // picker to reload since during initialization the list was
+            // still empty
+            self.eventPickerView.reloadAllComponents()
         })
-        
     }
     
-    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        for item in eventNames {
+            print(item)
+        }
     }
-
-
 }
-
