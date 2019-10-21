aa48578dccac42aa7b850d79ada1c63ea5cb648e
jliew1975 <jliew1975@gmail.com>
2019-10-20 22:13:03 -0400

    Intergrate with EasyOpenCv and DogeCv modules

---
+++ SkyStone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team12538/test/DogeCVDetectorTest.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.team12538.test;
+
+import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
+import com.disnodeteam.dogecv.detectors.skystone.StoneDetector;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Point;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp(name = "DogeCV Skystone Detection Test", group = "Test")
+public class DogeCVDetectorTest extends LinearOpMode {
+    enum DetectMode { Stone, SkyStone }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        /*
+         * Instantiate an OpenCvCamera object for the camera we'll be using.
+         * In this sample, we're using the phone's internal camera. We pass it a
+         * CameraDirection enum indicating whether to use the front or back facing
+         * camera, as well as the view that we wish to use for camera monitor (on
+         * the RC phone). If no camera monitor is desired, use the alternate
+         * single-parameter constructor instead (commented out below)
+         */
+        int phoneCameraMonitorViewId =
+                hardwareMap.appContext.getResources().getIdentifier(
+                        "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        OpenCvCamera phoneCam =
+                new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, phoneCameraMonitorViewId);
+
+        // Uncomment below if using webcam
+        /*
+        int webCameraMonitorViewId =
+            hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        OpenCvCamera webcam =
+                new OpenCvWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), webCameraMonitorViewId);
+        */
+
+        /*
+         * Open the connection to the camera device
+         */
+        phoneCam.openCameraDevice();
+
+        /*
+         * Specify the image processing pipeline we wish to invoke upon receipt
+         * of a frame from the camera. Note that switching pipelines on-the-fly
+         * (while a streaming session is in flight) *IS* supported.
+         */
+        StoneDetector stoneDetector = new StoneDetector();
+        SkystoneDetector skystoneDetector = new SkystoneDetector();
+
+        phoneCam.setPipeline(skystoneDetector);
+
+        /*
+         * Tell the camera to start streaming images to us! Note that you must make sure
+         * the resolution you specify is supported by the camera. If it is not, an exception
+         * will be thrown.
+         *
+         * Also, we specify the rotation that the camera is used in. This is so that the image
+         * from the camera sensor can be rotated such that it is always displayed with the image upright.
+         * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+         * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+         * away from the user.
+         */
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+        /*
+         * Wait for the user to press start on the Driver Station
+         */
+        waitForStart();
+
+        DetectMode mode = DetectMode.SkyStone;
+
+        while (opModeIsActive()) {
+            if(gamepad1.a && mode != DetectMode.Stone) {
+                mode = DetectMode.Stone;
+                phoneCam.setPipeline(stoneDetector);
+            } else if(gamepad1.b && mode != DetectMode.SkyStone) {
+                mode = DetectMode.SkyStone;
+                phoneCam.setPipeline(skystoneDetector);
+            }
+
+            /*
+             * Send some stats to the telemetry
+             */
+            telemetry.addData("Frame Count", phoneCam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", phoneCam.getFps()));
+            telemetry.addData("Total frame time ms", phoneCam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", phoneCam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", phoneCam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", phoneCam.getCurrentPipelineMaxFps());
+
+            if(mode == DetectMode.Stone && stoneDetector.foundScreenPositions().size() != 0d) {
+                for(Point point : stoneDetector.foundScreenPositions()) {
+                    printDetectorTelemetry(mode, point);
+                }
+            } else if(mode == DetectMode.SkyStone && skystoneDetector.foundRectangle().area() != 0d) {
+                printDetectorTelemetry(mode, skystoneDetector.getScreenPosition());
+            }
+
+            telemetry.update();
+
+
+            sleep(100);
+        }
+
+        phoneCam.closeCameraDevice();
+    }
+
+    private void printDetectorTelemetry(DetectMode mode, Point point) {
+        telemetry.addData(String.format("%s found", mode), true);
+        telemetry.addData("Skystone Point", point);
+    }
+}
