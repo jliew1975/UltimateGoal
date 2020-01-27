d60d84c82765c69bf29916da0305ffc3bea9eea4
Kevin Mathew <kevinjacobmathew2000@gmail.com>
2020-01-26 20:37:01 -0500

    changed kV

--- SkyStone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team12538/drive/DriveConstants.java
+++ SkyStone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team12538/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01461; // 0.01361  / rpmToVelocity(getMaxRpm());
-    public static double kA = 0.00013;
-    public static double kStatic = 0.07028;// 0.07292;
+    public static double kV = 1.0 / rpmToVelocity(getMaxRpm());
+    public static double kA = 0;
+    public static double kStatic = 0;// 0.07292;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
